6dcaf287f65167e1d0a86d48c398f13ccacbbd45
Gouthami Yerramsetty <gouthsetty@gmail.com>
2023-08-19 15:55:58 +0200

    Javascript Adnavced learning

---
+++ MapFilterProj/1.js
@@ -0,0 +1,29 @@
+/* https://onecompiler.com/javascript/3x56c9n36
+https://www.freecodecamp.org/news/higher-order-functions-in-javascript-explained/
+
+----------------------------------------------------------------------------------
+Apply all concepts  - Spread operator, rest operator, High order function 
+map(), reduce(), filter(), and sort()
+----------------------------------------------------------------------------------
+*/
+
+// Array of strings
+
+let mobiles = ["iPhone", "Samsung", "Pixel"];
+
+
+
+
+
+
+// Array of numbers
+const numbers = [1, 2, 3, 4];
+
+
+
+
+
+
+// array with mixed data types
+const newData = ['work', 'exercise', 1, true];
+
---
+++ MapFilterProj/2.js
@@ -0,0 +1,15 @@
+
+// Array of objects
+
+let students = [
+  { id: "001", name: "Anish", sports: "Cricket", age : 25},
+  { id: "002", name: "Smriti", sports: "Basketball" , age : 19},
+  { id: "003", name: "Rahul", sports: "Cricket", age : 20 },
+  { id: "004", name: "Bakul", sports: "Basketball", age : 15 },
+  { id: "005", name: "Nikita", sports: "Hockey" , age : 20},
+];
+
+
+
+// get the list of players who play cricket
+
