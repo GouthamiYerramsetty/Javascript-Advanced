015d1f447fbcc356facfd2aeb4b3528d6402fae4
Gouthami Yerramsetty <gouthsetty@gmail.com>
2023-08-19 16:31:49 +0200

    map and foreach initial example

--- MapFilterProj/1.js
+++ MapFilterProj/1.js
@@ -19,8 +19,26 @@ let mobiles = ["iPhone", "Samsung", "Pixel"];
 // Array of numbers
 const numbers = [1, 2, 3, 4];
 
+   /*
+     Difference between forEach and map - 
+      - 'forEach' will not return anything , it's job is to iterate the array and return anything ('undefined')
+      - 'map' method returns a new Array, and it's used to transform the input array according to the needs and requirements  
+   */
 
+    const eachOperationJustPrinting = numbers.forEach(num => console.log("eachOperationJustPrinting",num))
 
+    const eachOperation = numbers.forEach(num => {
+        return num
+    })
+      console.log("eachOperation",eachOperation);
+
+    const mapOperation_1 = numbers.map(num => num * 5)
+      console.log("mapOperation_1",mapOperation_1);
+
+    const mapOperation_2 = numbers.map(num => {
+        return num *2
+    })  
+      console.log("mapOperation_2",mapOperation_2);
 
 
 
